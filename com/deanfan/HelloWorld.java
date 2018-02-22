d6bc4ac2f5064800ca2477575aa898b0b96209cc
denafan <fan36hua36@163.com>
2018-02-22 17:30:17 +0800

    我提交的第一个文件

---
+++ com/deanfan/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.deanfan;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+			System.out.println("hello world2 !!!");
+			System.out.println("hello github !!!");
+	}
+}
